aa43b2623192fa2a997b7588c0ee2c9b3a65455c
serenityglass <97998517+serenityglass@users.noreply.github.com>
2024-07-07 17:44:42 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBplBqyrZpxRwA1k40I2tqRKDB_ZDfvtG0",
+  authDomain: "therapy-interface.firebaseapp.com",
+  projectId: "therapy-interface",
+  storageBucket: "therapy-interface.appspot.com",
+  messagingSenderId: "503153874415",
+  appId: "1:503153874415:web:f4568707cfc4cf4966d21a",
+  measurementId: "G-XX79XS50Y8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
